be12cf15d61110857c8e4f1251e4b7acb6bd2409
Philip Chimento <philip.chimento@gmail.com>
2021-12-12 21:16:42 -0800

    Day 4, Part 2

--- advent2021/src/puzzle4.rs
+++ advent2021/src/puzzle4.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 struct Board {
     rows: [HashSet<u8>; 10],
 }
@@ -36,6 +36,15 @@ impl Board {
         won
     }
 
+    fn has_won(&self) -> bool {
+        for ix in 0..10 {
+            if self.rows[ix].is_empty() {
+                return true;
+            }
+        }
+        false
+    }
+
     fn sum_unmarked(&self) -> u32 {
         let mut all_numbers: HashSet<u32> = HashSet::new();
         for ix in 0..5 {
@@ -54,30 +63,57 @@ pub fn main(is_part2: bool) {
         .split(',')
         .map(|s| s.parse::<u8>().unwrap());
     let mut boards: Vec<_> = parts.map(Board::from_string).collect();
-    if is_part2 {
-        panic!("no part 2 yet");
-    } else {
-        for draw in draws {
-            for board in &mut boards {
-                if board.mark(draw) {
+    let mut score_of_last_winning = None;
+    for draw in draws {
+        for board in &mut boards {
+            let already_won = board.has_won();
+            if board.mark(draw) {
+                if is_part2 {
+                    if !already_won {
+                        score_of_last_winning = Some(board.sum_unmarked() * draw as u32);
+                    }
+                } else {
                     println!("{}", board.sum_unmarked() * draw as u32);
                     return;
                 }
             }
         }
-        panic!("no board won");
+        if is_part2 && boards.iter().all(Board::has_won) {
+            println!("{}", score_of_last_winning.unwrap());
+            return;
+        }
     }
+    panic!("no board won");
 }
 
-#[test]
-fn board_from_grid() {
-    let board = Board::from_grid(&[
+#[cfg(test)]
+static EXAMPLE_GRIDS: [[[u8; 5]; 5]; 3] = [
+    [
         [22, 13, 17, 11, 0],
         [8, 2, 23, 4, 24],
         [21, 9, 14, 16, 7],
         [6, 10, 3, 18, 5],
         [1, 12, 20, 15, 19],
-    ]);
+    ],
+    [
+        [3, 15, 0, 2, 22],
+        [9, 18, 13, 17, 5],
+        [19, 8, 7, 25, 23],
+        [20, 11, 10, 24, 4],
+        [14, 21, 16, 12, 6],
+    ],
+    [
+        [14, 21, 17, 24, 4],
+        [10, 16, 15, 9, 19],
+        [18, 8, 23, 26, 20],
+        [22, 11, 13, 6, 5],
+        [2, 0, 12, 3, 7],
+    ],
+];
+
+#[test]
+fn board_from_grid() {
+    let board = Board::from_grid(&EXAMPLE_GRIDS[0]);
     assert_eq!(
         board.rows,
         [
@@ -107,48 +143,42 @@ fn board_from_string() {
  1 12 20 15 19\
     "
         ),
-        Board::from_grid(&[
-            [22, 13, 17, 11, 0],
-            [8, 2, 23, 4, 24],
-            [21, 9, 14, 16, 7],
-            [6, 10, 3, 18, 5],
-            [1, 12, 20, 15, 19],
-        ])
+        Board::from_grid(&EXAMPLE_GRIDS[0])
     );
 }
 
 #[test]
 fn part1_example() {
     let mut boards = [
-        Board::from_grid(&[
-            [22, 13, 17, 11, 0],
-            [8, 2, 23, 4, 24],
-            [21, 9, 14, 16, 7],
-            [6, 10, 3, 18, 5],
-            [1, 12, 20, 15, 19],
-        ]),
-        Board::from_grid(&[
-            [3, 15, 0, 2, 22],
-            [9, 18, 13, 17, 5],
-            [19, 8, 7, 25, 23],
-            [20, 11, 10, 24, 4],
-            [14, 21, 16, 12, 6],
-        ]),
-        Board::from_grid(&[
-            [14, 21, 17, 24, 4],
-            [10, 16, 15, 9, 19],
-            [18, 8, 23, 26, 20],
-            [22, 11, 13, 6, 5],
-            [2, 0, 12, 3, 7],
-        ]),
+        Board::from_grid(&EXAMPLE_GRIDS[0]),
+        Board::from_grid(&EXAMPLE_GRIDS[1]),
+        Board::from_grid(&EXAMPLE_GRIDS[2]),
     ];
     for draw in [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21] {
         for ix in 0..3 {
-            assert_eq!(boards[ix].mark(draw), false);
+            assert!(!boards[ix].mark(draw));
         }
     }
-    assert_eq!(boards[0].mark(24), false);
-    assert_eq!(boards[1].mark(24), false);
-    assert_eq!(boards[2].mark(24), true);
+    assert!(!boards[0].mark(24));
+    assert!(!boards[1].mark(24));
+    assert!(boards[2].mark(24));
     assert_eq!(boards[2].sum_unmarked(), 188);
+}
+
+#[test]
+fn part2_example() {
+    let mut boards = [
+        Board::from_grid(&EXAMPLE_GRIDS[0]),
+        Board::from_grid(&EXAMPLE_GRIDS[1]),
+        Board::from_grid(&EXAMPLE_GRIDS[2]),
+    ];
+    for draw in [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16] {
+        boards[0].mark(draw);
+        boards[2].mark(draw);
+        assert!(!boards[1].mark(draw));
+    }
+    assert!(boards[0].has_won());
+    assert!(boards[2].has_won());
+    assert!(boards[1].mark(13));
+    assert_eq!(boards[1].sum_unmarked(), 148);
 }
