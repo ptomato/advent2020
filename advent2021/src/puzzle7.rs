20efb1d4aa73ca500c0aaf37f4ef596145eff58f
Philip Chimento <philip.chimento@gmail.com>
2021-12-13 20:02:20 -0800

    Day 7, Part 2

--- advent2021/src/puzzle7.rs
+++ advent2021/src/puzzle7.rs
@@ -5,12 +5,17 @@ fn fuel_cost(positions: &Array1<i32>, target: i32) -> i32 {
     (positions - target).mapv(i32::abs).sum()
 }
 
-fn minimize_fuel(positions: &Array1<i32>) -> i32 {
+fn quadratic_fuel_cost(positions: &Array1<i32>, target: i32) -> i32 {
+    let dist = (positions - target).mapv(i32::abs);
+    (&dist * (&dist + 1) / 2).sum()
+}
+
+fn minimize_fuel(positions: &Array1<i32>, cost_function: &dyn Fn(&Array1<i32>, i32) -> i32) -> i32 {
     let (&min, &max) = positions.iter().minmax().into_option().unwrap();
-    fuel_cost(
+    cost_function(
         positions,
         (min..=max)
-            .min_by_key(|&pos| fuel_cost(positions, pos))
+            .min_by_key(|&pos| cost_function(positions, pos))
             .unwrap(),
     )
 }
@@ -23,7 +28,17 @@ pub fn main(is_part2: bool) {
             .map(|s| s.parse().unwrap())
             .collect::<Vec<_>>(),
     );
-    println!("{}", minimize_fuel(&positions));
+    println!(
+        "{}",
+        minimize_fuel(
+            &positions,
+            if is_part2 {
+                &quadratic_fuel_cost
+            } else {
+                &fuel_cost
+            }
+        )
+    );
 }
 
 #[test]
@@ -33,5 +48,13 @@ fn part1_example() {
     assert_eq!(fuel_cost(&positions, 2), 37);
     assert_eq!(fuel_cost(&positions, 3), 39);
     assert_eq!(fuel_cost(&positions, 10), 71);
-    assert_eq!(minimize_fuel(&positions), 37);
+    assert_eq!(minimize_fuel(&positions, &fuel_cost), 37);
+}
+
+#[test]
+fn part2_example() {
+    let positions = array![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+    assert_eq!(quadratic_fuel_cost(&positions, 2), 206);
+    assert_eq!(quadratic_fuel_cost(&positions, 5), 168);
+    assert_eq!(minimize_fuel(&positions, &quadratic_fuel_cost), 168);
 }
